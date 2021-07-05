cd3e7073cf38d1218288039100c7a99d4f33450a
Lane2002 <woodelaine2002@hotmail.co.uk>
2021-07-05 12:49:26 +0100

    first commit

---
+++ files/index.php
@@ -0,0 +1,21 @@
+<!-- Apply your understanding of handling HTML forms in PHP to create a calculator. -->
+
+<html>
+  <body>
+<!--Your code goes here-->
+    <h2>Addition</h2>
+    <form action="addition.php" method="get">
+      First number: <input type="number" name="add_first"><br>
+      Second number: <input type="number" name="add_second"><br>
+      <button type="submit">ADD</button>
+    </form>
+    
+    <h2>Dividion</h2>
+    <form action="division.php" method="get">
+      First number: <input type="number" name="div_first"><br>
+      Second number: <input type="number" name="div_second"><br>
+      <button type="submit">DIVIDE</button>
+    </form>
+    <a href="index.php">Reset</a>
+  </body>
+</html>
